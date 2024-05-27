e63a153edb0c383b7f3a67dae3fbaf716b24cd81
Edanriell <Edanriell@gmail.com>
2024-05-27 22:37:17 +0300

    splitted component

--- client/src/entities/results/ui/ui.svelte
+++ client/src/entities/results/ui/ui.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { css } from "@emotion/css";
+
+	import { Button } from "@shared/ui/button";
 </script>
 
 <article class={css`
@@ -439,19 +441,7 @@
 				</dd>
 			</div>
 		</dl>
-		<button class={css`
-			width: 100%;
-			padding-top: 1.7rem;
-			padding-bottom: 1.6rem;
-			border-radius: 12.8rem;
-			background: var(--dark-navy);
-			font-family: var(--font-family), sans-serif;
-			font-weight: 700;
-			font-size: 1.8rem;
-			text-align: center;
-			color: var(--white);
-			cursor:pointer;
-		`} type="button">Continue</button>
+		<Button />
 	</div>
 </article>
 
---
+++ client/src/shared/ui/button/ui.svelte
@@ -0,0 +1,21 @@
+<script>
+	import {css} from "@emotion/css";
+</script>
+
+<button
+	class={css`
+		width: 100%;
+		padding-top: 1.7rem;
+		padding-bottom: 1.6rem;
+		border-radius: 12.8rem;
+		background: var(--dark-navy);
+		font-family: var(--font-family), sans-serif;
+		font-weight: 700;
+		font-size: 1.8rem;
+		text-align: center;
+		color: var(--white);
+		cursor:pointer;
+	`}
+    type="button">
+	Continue
+</button>

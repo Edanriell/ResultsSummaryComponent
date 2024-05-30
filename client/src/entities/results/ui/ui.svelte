31bb1f0bcf6ca78a94e435c6dffc92a772bbaf51
Edanriell <Edanriell@gmail.com>
2024-05-30 19:06:16 +0300

    working on component

--- client/src/entities/results/ui/ui.svelte
+++ client/src/entities/results/ui/ui.svelte
@@ -11,12 +11,50 @@
 
 	let summaryResult: Result;
 	let averageScore: number;
+	let resultComponentBackgroundColor: string;
+	let resultComponentResultMessage: string;
+	let resultComponentScoreResultMessage: string;
+	let resultComponentTextColor: string;
+	// Need adjust colors for text
+	// Add greenish, yellowish etc. color shades
 
 	onMount(async () => {
 		const { result } = await getResultQuery();
 		summaryResult = result;
 		averageScore = calculateAverageScore(summaryResult);
+		updateResultComponentContent();
 	})
+
+	const updateResultComponentContent = () => {
+		// Function must return object, so it can be putted in models {}
+
+		if (averageScore <= 20) {
+			resultComponentBackgroundColor = 'linear-gradient(180deg, #ff7f7f 0%, #ff4c4c 0.01%, #e93f3f 100%)';
+			resultComponentResultMessage = 'Your performance is below average. Keep practicing!';
+			resultComponentScoreResultMessage = 'Needs Improvement';
+			resultComponentTextColor = '#fff';
+		} else if (averageScore <= 40) {
+			resultComponentBackgroundColor = 'linear-gradient(180deg, #ffaf7f 0%, #ff8c4c 0.01%, #e96f3f 100%)';
+			resultComponentResultMessage = "Your performance is average. There's room for improvement.";
+			resultComponentScoreResultMessage = 'Average';
+			resultComponentTextColor = '#fff';
+		} else if (averageScore <= 60) {
+			resultComponentBackgroundColor = 'linear-gradient(180deg, #fff75f 0%, #ffd94c 0.01%, #e9b93f 100%)';
+			resultComponentResultMessage = 'Your performance is above average. Keep up the good work!';
+			resultComponentScoreResultMessage = 'Great';
+			resultComponentTextColor = '#000';
+		} else if (averageScore <= 80) {
+			resultComponentBackgroundColor = 'linear-gradient(180deg, #75f 0%, #6943ff 0.01%, #2f2ce9 100%)';
+			resultComponentResultMessage = 'Your performance exceeds 65% of the people conducting the test here!';
+			resultComponentScoreResultMessage = 'Excellent';
+			resultComponentTextColor = '#cac9ff';
+		} else {
+			resultComponentBackgroundColor = 'linear-gradient(180deg, #7fffaf 0%, #4cff8c 0.01%, #3fe96f 100%)';
+			resultComponentResultMessage = "Your performance is exceptional! You're among the top performers.";
+			resultComponentScoreResultMessage = 'Outstanding';
+			resultComponentTextColor = '#000';
+		}
+	};
 </script>
 
 <article class={css`
@@ -28,11 +66,11 @@
 			justify-content: center;
 		}
 	`}>
-	<div class={css`
+	<div style="background: {resultComponentBackgroundColor}" class={css`
 			padding: 2.4rem 3rem 4rem;
 			border-radius: 0 0 3.2rem 3.2rem;
 			box-shadow: 0 30px 60px 0 rgba(61, 108, 236, 0.15);
-			background: linear-gradient(180deg, #75f 0%, #6943ff 0.01%, #2f2ce9 100%);
+			//background: linear-gradient(180deg, #75f 0%, #6943ff 0.01%, #2f2ce9 100%);
 			display: flex;
 			flex-direction: column;
 			align-items: center;
@@ -45,12 +83,12 @@
 				flex: 0 0;
 			}
 		`}>
-		<h2 class={css`
+		<h2 style="color: {resultComponentTextColor}" class={css`
 			font-family: var(--font-family), sans-serif;
 			font-weight: 700;
 			font-size: 1.8rem;
 			text-align: center;
-			color: var(--light-blue);
+			// color: var(--light-blue);
 			margin-bottom: 2.4rem;
 			@media (width >= 768px) {
 				font-size: 2.4rem;
@@ -69,6 +107,207 @@
 				margin-bottom: 2.8rem;
 			}
 		`}>
+			{#if averageScore <= 20}
+				<svg
+					class={css`
+				    position: absolute;
+				    top: 0;
+				    left: 0;
+				    z-index: 1;
+				    @media (width >= 768px) {
+				        display: none;
+				    }
+				`}
+					width="140" height="140" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="70" cy="70" r="70" fill="url(#paint0_linear_54_23)" />
+					<defs>
+						<linearGradient id="paint0_linear_54_23" x1="70" y1="0" x2="70" y2="140" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#ff7f7f" />
+							<stop offset="1" stop-color="#e93f3f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				<svg
+					class={css`
+					    position: absolute;
+					    top: 0;
+					    left: 0;
+					    z-index: 1;
+					    display: none;
+					    @media (width >= 768px) {
+					        display: block;
+					    }
+                    `}
+						width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="100" cy="100" r="100" fill="url(#paint0_linear_14_43)" />
+					<defs>
+						<linearGradient id="paint0_linear_14_43" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#ff7f7f" />
+							<stop offset="1" stop-color="#e93f3f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				{:else if averageScore > 20 && averageScore <= 40 }
+				<svg
+					class={css`
+				        position: absolute;
+				        top: 0;
+				        left: 0;
+				        z-index: 1;
+				        @media (width >= 768px) {
+				            display: none;
+				        }
+			    `}
+					width="140" height="140" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="70" cy="70" r="70" fill="url(#paint0_linear_54_23)" />
+					<defs>
+						<linearGradient id="paint0_linear_54_23" x1="70" y1="0" x2="70" y2="140" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#ffaf7f" />
+							<stop offset="1" stop-color="#e96f3f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				<svg
+				class={css`
+				    position: absolute;
+				    top: 0;
+				    left: 0;
+				    z-index: 1;
+				    display: none;
+				    @media (width >= 768px) {
+				        display: block;
+				    }
+    			`}
+					width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="100" cy="100" r="100" fill="url(#paint0_linear_14_43)" />
+					<defs>
+						<linearGradient id="paint0_linear_14_43" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#ffaf7f" />
+							<stop offset="1" stop-color="#e96f3f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				{:else if averageScore > 41 && averageScore <= 60}
+				<svg
+				class={css`
+					position: absolute;
+					top: 0;
+					left: 0;
+					z-index: 1;
+					@media (width >= 768px) {
+					    display: none;
+					}
+				`}
+					width="140" height="140" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="70" cy="70" r="70" fill="url(#paint0_linear_54_23)" />
+					<defs>
+						<linearGradient id="paint0_linear_54_23" x1="70" y1="0" x2="70" y2="140" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#fff75f" />
+							<stop offset="1" stop-color="#e9b93f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				<svg
+				class={css`
+				    position: absolute;
+				    top: 0;
+				    left: 0;
+				    z-index: 1;
+				    display: none;
+				    @media (width >= 768px) {
+				        display: block;
+				    }
+    			`}
+					width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="100" cy="100" r="100" fill="url(#paint0_linear_14_43)" />
+					<defs>
+						<linearGradient id="paint0_linear_14_43" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#fff75f" />
+							<stop offset="1" stop-color="#e9b93f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				{:else if averageScore > 60 && averageScore <= 80}
+				<svg
+				class={css`
+			        position: absolute;
+			        top: 0;
+			        left: 0;
+			        z-index: 1;
+			        @media (width >= 768px) {
+			            display: none;
+			        }
+			    `}
+					width="140" height="140" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="70" cy="70" r="70" fill="url(#paint0_linear_54_23)" />
+					<defs>
+						<linearGradient id="paint0_linear_54_23" x1="70" y1="0" x2="70" y2="140" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#75f" />
+							<stop offset="1" stop-color="#2f2ce9" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				<svg
+				class={css`
+			        position: absolute;
+			        top: 0;
+			        left: 0;
+			        z-index: 1;
+			        display: none;
+			        @media (width >= 768px) {
+			            display: block;
+			        }
+   				 `}
+					width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="100" cy="100" r="100" fill="url(#paint0_linear_14_43)" />
+					<defs>
+						<linearGradient id="paint0_linear_14_43" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#75f" />
+							<stop offset="1" stop-color="#2f2ce9" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				{:else if averageScore > 80 && averageScore <= 100}
+				<svg
+				class={css`
+			        position: absolute;
+			        top: 0;
+			        left: 0;
+			        z-index: 1;
+			        @media (width >= 768px) {
+			            display: none;
+			        }
+			    `}
+					width="140" height="140" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="70" cy="70" r="70" fill="url(#paint0_linear_54_23)" />
+					<defs>
+						<linearGradient id="paint0_linear_54_23" x1="70" y1="0" x2="70" y2="140" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#7fffaf" />
+							<stop offset="1" stop-color="#3fe96f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+				<svg
+				class={css`
+				    position: absolute;
+				    top: 0;
+				    left: 0;
+				    z-index: 1;
+				    display: none;
+				    @media (width >= 768px) {
+				        display: block;
+				    }
+   				 `}
+					width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<circle cx="100" cy="100" r="100" fill="url(#paint0_linear_14_43)" />
+					<defs>
+						<linearGradient id="paint0_linear_14_43" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse">
+							<stop stop-color="#7fffaf" />
+							<stop offset="1" stop-color="#3fe96f" stop-opacity="0" />
+						</linearGradient>
+					</defs>
+				</svg>
+			{/if}
 			<svg
 				class={css`
 					position: absolute;
@@ -154,13 +393,13 @@
 				font-size: 3.2rem;
 				margin-bottom: 1.4rem;
 			}
-		`}>Great</p>
-		<p class={css`
+		`}>{resultComponentScoreResultMessage}</p>
+		<p style="color: {resultComponentTextColor}" class={css`
 			font-family: var(--font-family), sans-serif;
 			font-weight: 500;
 			font-size: 16px;
 			text-align: center;
-			color: var(--light-blue);
+			// color: var(--light-blue);
 			@media (width >= 768px) {
 				font-size: 1.8rem;
 				min-width: 26rem;

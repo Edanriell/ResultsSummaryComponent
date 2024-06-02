f67077f29669fce3ff0e963f51f3848598715c6c
Edanriell <Edanriell@gmail.com>
2024-06-02 19:49:12 +0300

    minor server refactors

---
+++ server/server/Dto/ResultDto.cs
@@ -0,0 +1,8 @@
+using Server.Entities;
+
+namespace Server.Dto;
+
+public class ResultDto
+{
+    public Result Result { get; set; } = null!;
+}
---
+++ server/server/Entities/Result.cs
@@ -0,0 +1,9 @@
+namespace Server.Entities;
+
+public class Result
+{
+    public byte ReactionScore { get; set; }
+    public byte MemoryScore { get; set; }
+    public byte VerbalScore { get; set; }
+    public byte VisualScore { get; set; }
+}
